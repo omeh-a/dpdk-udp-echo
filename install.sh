52d2001446e9a01846cc09fe7162b69b246d9b15
Matt Rossouw <39023043+omeh-a@users.noreply.github.com>
2023-07-27 11:31:52 +1000

    Note added to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-
+# Note: this script will do absolutely nothing unless you are on my network.
+#       adjust the following to scp to your target machine if you want to use it.
 make && scp ./udpecho ./utilisationsocket ertos@vb-103:~/benchmarking/
